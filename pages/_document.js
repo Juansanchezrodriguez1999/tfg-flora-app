8b8f24591df0e56e5a403fe1a4077381ef4234d3
juansanchezrodriguez1999 <global-juansanchezrodriguez1999@uma.es>
2023-03-10 08:38:20 +0000

    upload new images and fix url in manifest and _documents

--- pages/_document.js
+++ pages/_document.js
@@ -5,17 +5,18 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <meta name="application-name" content="EnBiC2-Lab App" />
+          <link rel="manifest" href="/services/flora/app/manifest.json" />
+          <meta name="application-name" content="Flora App" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
           />
-          <meta name="apple-mobile-web-app-title" content="EnBiC2-Lab App" />
-          <meta name="description" content="EnBiC2-Lab App from" />
+          <meta name="apple-mobile-web-app-title" content="Flora App" />
+          <meta name="description" content="Flora App" />
           <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <link rel="manifest" href="/manifest.json" />
+          
           <meta
             name="msapplication-config"
             content="/icons/browserconfig.xml"
@@ -23,9 +24,8 @@ class MyDocument extends Document {
           <meta name="msapplication-TileColor" content="#2B5797" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
-          <link rel="manifest" href="/manifest.json" />
-
           <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+
           <link
             rel="apple-touch-icon"
             sizes="152x152"
@@ -40,6 +40,18 @@ class MyDocument extends Document {
             rel="apple-touch-icon"
             sizes="167x167"
             href="/icons/touch-icon-ipad-retina.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/services/flora/app/logo.png"
+          />
+          <link
+            rel="icon"
+            type="image/x-icon"
+            sizes="192x192"
+            href="/services/flora/app/favicon.png"
           />
           <link
             rel="icon"
@@ -59,10 +71,11 @@ class MyDocument extends Document {
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://yourdomain.com" />
-          <meta name="twitter:title" content="PWA App" />
+          <meta name="twitter:title" content="Flora App" />
+          <meta name="twitter:description" content="Flora app" />
           <meta
             name="twitter:description"
-            content="Best PWA App in the world"
+            content="Flora App"
           />
           <meta
             name="twitter:image"
@@ -70,15 +83,14 @@ class MyDocument extends Document {
           />
           <meta name="twitter:creator" content="@DavidWShadow" />
           <meta property="og:type" content="website" />
-          <meta property="og:title" content="PWA App" />
-          <meta property="og:description" content="Best PWA App in the world" />
-          <meta property="og:site_name" content="PWA App" />
+          <meta property="og:title" content="Flora App" />
+          <meta property="og:description" content="Flora App" />
+          <meta property="og:site_name" content="Flora App" />
           <meta property="og:url" content="https://yourdomain.com" />
           <meta
             property="og:image"
             content="https://yourdomain.com/icons/apple-touch-icon.png"
           />
-          
         </Head>
         <body>
           <Main />
